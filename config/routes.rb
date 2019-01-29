efe88d900ad502a4e5b6b496c9841b11f7704350
Lmadushan <lahiruthebest1@gmail.com>
2019-01-29 17:35:30 +0530

    Added testing and factories

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+
+	# Route for root
+	root 'pages#index'
+
+	# Route for About Page 
+	get 'about' => 'pages#about'
+
+	#Resource route for posts
+	resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
